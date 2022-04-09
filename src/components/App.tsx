19454fd7cb95b2083c86535cded2b8e2e2b0a698
cfrick16 <cfrick16@gmail.com>
2022-04-09 17:54:13 -0500

    Initial Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import TopBar from "./TopBar";
+
+function App() {
+  return (
+    <div className="App">
+        <TopBar/>
+    </div>
+  );
+}
+
+export default App;
