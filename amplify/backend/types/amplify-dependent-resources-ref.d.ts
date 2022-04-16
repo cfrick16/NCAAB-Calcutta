75575d2f3b34007446e008e3118b569c9895faed
cfrick16 <cfrick16@gmail.com>
2022-04-16 15:24:16 -0500

    Fixing Amplify build

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,28 @@
 export type AmplifyDependentResourcesAttributes = {
-    'auth': {
-        'nflcalcutta15eb7e86': {
-            'IdentityPoolId': 'string',
-            'IdentityPoolName': 'string',
-            'UserPoolId': 'string',
-            'UserPoolArn': 'string',
-            'UserPoolName': 'string',
-            'AppClientIDWeb': 'string',
-            'AppClientID': 'string'
+    "auth": {
+        "nflcalcutta15eb7e86": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
         }
     },
-    'api': {
-        'groupsApi': {
-            'RootUrl': 'string',
-            'ApiName': 'string',
-            'ApiId': 'string'
+    "api": {
+        "groupsApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
         }
     },
-    'function': {
-        'groupsLambda': {
-            'Name': 'string',
-            'Arn': 'string',
-            'Region': 'string',
-            'LambdaExecutionRole': 'string'
+    "function": {
+        "groupsLambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
